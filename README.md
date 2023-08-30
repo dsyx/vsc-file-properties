6a9c590c0b04c3d157981618ac043c07a85d79cb
Yaoxing Shan <shanyaoxing12@outlook.com>
2023-08-30 11:29:57 +0800

    feat: add some configuration options

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ A VSCode extension for conveniently displaying file properties.
 ## Features
 
 - Show active file properties in status bar, such as permissions, size, and modification time.
+- Click the status entry to view detailed properties of the file.
 
 ## Requirements
 
@@ -12,7 +13,11 @@ A VSCode extension for conveniently displaying file properties.
 
 ## Extension Settings
 
-N/A
+- `file-properties.showPermissionsInStatusBar`: Show file permissions in status bar.
+- `file-properties.showSizeInStatusBar`: Show file size in status bar.
+- `file-properties.showMtimeInStatusBar`: Show file last modified date in status bar.
+- `file-properties.useSiSizeUnit`: Use the size unit defined in SI (1 KB = 1000 B) instead of the definition in the IEC standard (1 KiB = 1024 B).
+- `file-properties.dateFormat`: For the format of all dates, see: https://moment.github.io/luxon/#/formatting?id=table-of-tokens.
 
 ## Known Issues
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,22 @@
-// The module 'vscode' contains the VS Code extensibility API
+// The module "vscode" contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import * as fs from "fs";
 import * as util from "util";
+import * as luxon from "luxon";
 
 const stat = util.promisify(fs.stat);
-const outputChannel = vscode.window.createOutputChannel('File Properties');
+const outputChannel = vscode.window.createOutputChannel("File Properties");
 const viewDetailCommand = vscode.commands.registerCommand("file-properties.viewDetail", viewDetailCommandHandler);
 const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+
+let configurations = {
+	showPermissionsInStatusBar: false,
+	showSizeInStatusBar: false,
+	showMtimeInStatusBar: false,
+	useSiSizeUnit: false,
+	dateFormat: "yyyy-MM-dd HH:mm:ss",
+};
 
 class ActiveEditorUndefinedError extends Error {
 	constructor() {
@@ -25,39 +34,60 @@ class ActiveEditorDocumentNotFileError extends Error {
 
 function formatPermissions(mode: number): string {
 	const permissions = [
-		(mode & 0o0400 ? 'r' : '-'),
-		(mode & 0o0200 ? 'w' : '-'),
-		(mode & 0o0100 ? 'x' : '-'),
-		(mode & 0o0040 ? 'r' : '-'),
-		(mode & 0o0020 ? 'w' : '-'),
-		(mode & 0o0010 ? 'x' : '-'),
-		(mode & 0o0004 ? 'r' : '-'),
-		(mode & 0o0002 ? 'w' : '-'),
-		(mode & 0o0001 ? 'x' : '-'),
+		(mode & 0o0400 ? "r" : "-"),
+		(mode & 0o0200 ? "w" : "-"),
+		(mode & 0o0100 ? "x" : "-"),
+		(mode & 0o0040 ? "r" : "-"),
+		(mode & 0o0020 ? "w" : "-"),
+		(mode & 0o0010 ? "x" : "-"),
+		(mode & 0o0004 ? "r" : "-"),
+		(mode & 0o0002 ? "w" : "-"),
+		(mode & 0o0001 ? "x" : "-"),
 	];
 
-	return permissions.join('');
+	return permissions.join("");
 }
 
-function formatFileSize(size: number): string {
-	const kilo = 1024;
-	const units = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+function formatSize(size: number): string {
+	const iecDefinition = {
+		units: ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"],
+		kilo: 1024,
+	};
+	const siDefinition = {
+		units: ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"],
+		kilo: 1000,
+	};
+
+	const def = configurations.useSiSizeUnit ? siDefinition : iecDefinition;
 
 	let index = 0;
-	while (size >= kilo && index < units.length - 1) {
-		size /= kilo;
+	while (size >= def.kilo && index < def.units.length - 1) {
+		size /= def.kilo;
 		index++;
 	}
 
-	return `${size.toFixed(2)} ${units[index]}`;
+	return `${size.toFixed(2)} ${def.units[index]}`;
+}
+
+function formatDate(date: Date): string {
+	const format = configurations.dateFormat;
+	return luxon.DateTime.fromJSDate(date).toFormat(format);
 }
 
 function formatStats(stats: fs.Stats): string {
-	const permissions = formatPermissions(stats.mode);
-	const size = formatFileSize(stats.size);
-	const mtime = stats.mtime.toISOString();
+	const properties = [];
 
-	return `[${permissions} | ${size} | ${mtime}]`;
+	if (configurations.showPermissionsInStatusBar) {
+		properties.push(formatPermissions(stats.mode));
+	}
+	if (configurations.showSizeInStatusBar) {
+		properties.push(formatSize(stats.size));
+	}
+	if (configurations.showMtimeInStatusBar) {
+		properties.push(formatDate(stats.mtime));
+	}
+
+	return `[${properties.join(" | ")}]`;
 }
 
 function getActiveFilePath(): string {
@@ -125,7 +155,7 @@ async function updateStatusBar() {
 		const properties = formatStats(stats);
 
 		statusBar.text = properties;
-		statusBar.tooltip = "File Properties: Click to view detailed file properties";
+		statusBar.tooltip = "Click to view detailed file properties";
 		statusBar.command = "file-properties.viewDetail";
 		statusBar.show();
 	} catch (error) {
@@ -133,9 +163,30 @@ async function updateStatusBar() {
 	}
 }
 
+function updateAllConfigurations() {
+	const config = vscode.workspace.getConfiguration("file-properties");
+
+	configurations.showPermissionsInStatusBar = config.get("showPermissionsInStatusBar") as boolean;
+	configurations.showSizeInStatusBar = config.get("showSizeInStatusBar") as boolean;
+	configurations.showMtimeInStatusBar = config.get("showMtimeInStatusBar") as boolean;
+	configurations.useSiSizeUnit = config.get("useSiSizeUnit") as boolean;
+	configurations.dateFormat = config.get("dateFormat") as string;
+
+	updateStatusBar();
+}
+
+function didChangeConfigurationHandler(e: vscode.ConfigurationChangeEvent) {
+	if (e.affectsConfiguration("file-properties")) {
+		updateAllConfigurations();
+	}
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	updateAllConfigurations();
+	vscode.workspace.onDidChangeConfiguration(didChangeConfigurationHandler);
+
 	context.subscriptions.push(viewDetailCommand);
 
 	updateStatusBar();
